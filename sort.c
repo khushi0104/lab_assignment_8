5572dbce8b32fd17642dc81d418778e38120f06b
khushi0104 <80493807+khushi0104@users.noreply.github.com>
2023-04-07 19:52:26 -0400

    Update sort.c
    heapSort and mergeSort filled in

--- sort.c
+++ sort.c
@@ -1,21 +1,113 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
+void heapify(int arr[], int n, int i) 
+{
+	int largest = i; 
+	int left = 2 * i + 1; 
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[largest]) {
+		largest = left;
+	}
+	if (right < n && arr[right] > arr[largest]) {
+		largest = right;
+	}
+
+	if (i != largest) {
+		int temp = arr[i];
+		arr[i] = arr[largest];
+		arr[largest] = temp;
+
+		heapify(arr, n, largest);
+
+	}
+
+}
 void heapSort(int arr[], int n)
 {
+	for (int i = n/2-1; i >= 0; i--) {
+		heapify(arr, n, i);
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		int temp = arr[0];
+		arr[0] = arr[i];
+		arr[i] = temp;
+
+		heapify(arr, i, 0);
+	}
 }
 
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l, int m, int r) {
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int *L = (int*) malloc(n1*sizeof(int));
+	extraMemoryAllocated += sizeof(L);
+	int *R = (int*) malloc(n2*sizeof(int));
+	extraMemoryAllocated += sizeof(R);
+
+	for (i = 0; i < n1; i++) {
+		L[i] = pData[l+i];
+	}
+	for (j = 0; j < n2; j++) {
+		R[j] = pData[m+1+j];
+	}
+
+	i = 0; 
+	j = 0;
+	k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			pData[k] = L[i];
+			i++;
+		}
+		else {
+			pData[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		pData[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		pData[k] = R[j];
+		j++;
+		k++;
+	}
+
+	free(L);
+	free(R);
+}
+
 void mergeSort(int pData[], int l, int r)
 {
+	if ( l < r) {
+		int m = (l+r)/2;
+		
+		mergeSort(pData, l, m);
+		mergeSort(pData, m+1, r);
+		
+		merge(pData, l, m, r);
+	}
 }
+
 
 // parses input file to an integer array
 int parseData(char *inputFileName, int **ppData)
@@ -53,16 +145,28 @@ void printArray(int pData[], int dataSz)
 {
 	int i, sz = dataSz - 100;
 	printf("\tData:\n\t");
-	for (i=0;i<100;++i)
+	if (dataSz > 100) 
 	{
-		printf("%d ",pData[i]);
-	}
-	printf("\n\t");
+		for (i=0;i<100;++i)
+		{
+			printf("%d ",pData[i]);
+		}
+		printf("\n\t");
 	
-	for (i=sz;i<dataSz;++i)
-	{
-		printf("%d ",pData[i]);
+		for (i=sz;i<dataSz;++i)
+		{
+			printf("%d ",pData[i]);
+		}
 	}
+	else 
+	{
+		for (i=0;i<dataSz;++i)
+		{
+			printf("%d ",pData[i]);
+		}
+		printf("\n\t");
+	}
+	
 	printf("\n\n");
 }
 
@@ -82,7 +186,7 @@ int main(void)
 			continue;
 		
 		pDataCopy = (int *)malloc(sizeof(int)*dataSz);
-	
+
 		printf("---------------------------\n");
 		printf("Dataset Size : %d\n",dataSz);
 		printf("---------------------------\n");
